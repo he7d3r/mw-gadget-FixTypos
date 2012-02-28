80bfa3eab7bf5fffcc7fefdfd6c39006c76f6e8a
Helder <he7d3r@gmail.com>
2012-02-28 12:21:28 +0000

    Update comment to reflect the current location of my scripts

--- src/FixTypos.js
+++ src/FixTypos.js
@@ -3,7 +3,7 @@
  * (Simplificação do [[w:en:User:Cacycle/wikEd.js]])
  * @author: [[w:en:User:Cacycle/wikEd.js]]
  * @author: [[User:Helder.wiki]]
- * @source: [[Special:GlobalUsage/User:Helder.wiki/Scripts/FixTypos.js]] ([[File:User:Helder.wiki/Scripts/FixTypos.js]])
+ * @source: [[Special:GlobalUsage/User:Helder.wiki/Tools/FixTypos.js]] ([[File:User:Helder.wiki/Tools/FixTypos.js]])
  */
 var	typoRulesFind = [],
 	typoRulesReplace = [];
