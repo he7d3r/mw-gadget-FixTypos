9ffddd55dc5d852916e9a4215c39dfdacbc7f421
Helder <he7d3r@gmail.com>
2012-03-16 17:37:23 +0000

    Simplify even more for debugging

--- src/FixTypos.js
+++ src/FixTypos.js
@@ -12,10 +12,7 @@ function processText( text ) {
 	}
 }
 
-/**
- * Load and parse RegExTypoFix rules
- */
-function loadTypoFixRules( page ) {
+function getWikiText( page ) {
 	var api = new mw.Api();
 	api.get( {
 		action: 'query',
@@ -26,19 +23,12 @@ function loadTypoFixRules( page ) {
 		titles: page
 	}, {
 		ok: function ( data ) {
-			var     q = data.query,
-				id = q && q.pageids && q.pageids[0],
-				pg = id && q.pages && q.pages[ id ],
-				rv = pg && pg.revisions;
-			if ( rv && rv[0] && rv[0]['*'] ) {
-				processText( rv[0]['*'] );
-			}
-		}
+			processText( data.query.pages[ data.query.pageids[0] ].revisions[0]['*'] );		}
 	} );
 }
 
 if( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ]) !== -1 ) {
 	mw.loader.using( ['mediawiki.api', 'user.options' ], function () {
-		loadTypoFixRules( 'Project:AutoWikiBrowser/Typos' );
+		getWikiText( 'Project:AutoWikiBrowser/Typos' );
 	} );
 }
