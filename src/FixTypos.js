3a15913426e66fead9dd8f7855f07a8a5576efc7
Helder <he7d3r@gmail.com>
2012-03-16 17:47:43 +0000

    Revert to version from 2012-03-06T19:00:04

--- src/FixTypos.js
+++ src/FixTypos.js
@@ -1,20 +1,112 @@
+/*
+ * Insere um botão na barra de ferramentas de edição para realizar correções tipográficas
+ * (Simplificação do [[w:en:User:Cacycle/wikEd.js]])
+ * @author: [[w:en:User:Cacycle/wikEd.js]]
+ * @author: [[User:Helder.wiki]]
+ * @source: [[Special:GlobalUsage/User:Helder.wiki/Tools/FixTypos.js]] ([[File:User:Helder.wiki/Tools/FixTypos.js]])
+ */
+var	typoRulesFind = [],
+	typoRulesReplace = [];
+
+/**
+ * Fix typos using the AutoWikiBrowser/RegExTypoFix list (.test() is not faster)
+ */ 
+function fixTypos(text) {
+	//	split into alternating plain text and { {lang} } template fragments (does not support nested templates)
+	var	i, j, fragment = [],
+		nextPos = 0,
+		regExp = /\{\{\s*lang\s*\|(.|\n)*?\}\}/gi;
+	while ( (regExpMatch = regExp.exec(text)) !== null ) {
+		fragment.push(text.substring(nextPos, regExpMatch.index));
+		fragment.push(regExpMatch[0]);
+		nextPos = regExp.lastIndex;
+	}
+	fragment.push(text.substring(nextPos));
+ 
+	// cycle through the RegExTypoFix rules
+	for ( i = 0; i < typoRulesFind.length; i ++) {
+		// cycle through the fragments, jump over { {lang} } templates
+		for ( j = 0; j < fragment.length; j = j + 2) {
+			fragment[j] = fragment[j].replace(typoRulesFind[i], typoRulesReplace[i]);
+		}
+	} 
+	// re-assemble text
+	return fragment.join('');
+}
+
 function addButton() {
-	console.debug('The code inside of addButton() was executed.');
-	return false;
+    var $edit = $( '#wpTextbox1' );
+	if( typeof $edit.wikiEditor !== 'function' ) {
+		return;
+	}
+	$edit.wikiEditor( 'addToToolbar', {
+		'section': 'main',
+		'group': 'insert',
+		'tools': {
+			'fix-typo': {
+				label: 'Corrigir erros tipográficos',
+				type: 'button',
+				icon: '//upload.wikimedia.org/wikipedia/commons/9/94/WikEd_fix_reg-ex-typo.png',
+				action: {
+					type: 'callback',
+					execute: function() {
+						var	text = $edit.val(),
+							newText = fixTypos( text );
+						if ( newText === text ) {
+							return;
+						}
+						$edit.val( newText );
+						$( '#wpMinoredit' ).attr('checked', true);
+						$( '#wpSummary' ).val( 'Correção de [[m:w:pt:WP:AWBT|erros tipográficos]]' );
+						$( '#wpDiff' ).click();
+					}
+				}
+			}
+		}
+	} );
 }
 
 function processText( text ) {
-	/* Check if the required modules are available and then customize the toolbar */
-	if ( mw.user.options.get('usebetatoolbar') ) {
-		mw.loader.using( 'ext.wikiEditor.toolbar', function () {
-			console.debug('"$( addButton )" was executed inside of mw.loader.using( \'ext.wikiEditor.toolbar\'...');
-			$( addButton );
-			return false;
-		} );
+	// parse regexp rules
+	regExp = /<(?:Typo)?\s+(?:word="(.*?)"\s+)?find="(.*?)"\s+replace="(.*?)"\s*\/?>/g;
+	while ( (regExpMatch = regExp.exec( text )) !== null) {
+		// check if this is a valid regexp
+		var regExpFind;
+		try {
+			regExpFind = new RegExp(regExpMatch[2], 'gm');
+		} catch (err) {
+			var msg = 'Expressão regular inválida:\nlocalizar=' +
+					regExpMatch[2] + '\nsubstituir=' + regExpMatch[3];
+			mw.log( msg );
+			continue;
+		}
+
+		// save regexp and replace
+		typoRulesFind.push(regExpFind);
+		typoRulesReplace.push(regExpMatch[3]);
+	}
+
+	// display typo fix button
+	if (typoRulesFind.length > 0) {
+		/* Check if we are in edit mode and the required modules are available and then customize the toolbar */
+		if ($.inArray(mw.config.get('wgAction'), ['edit', 'submit']) !== -1 ) {
+			if ( mw.user.options.get('usebetatoolbar') ) {
+				mw.loader.using( 'ext.wikiEditor.toolbar', function () {
+					$( addButton );
+				} );
+			} /* else {
+				// TODO: Add the button to the old toolbar
+			} */
+		}
+	} else {
+		mw.log( 'A lista de regras de correções tipográficas está vazia.' );
 	}
 }
 
-function getWikiText( page ) {
+/**
+ * Load and parse RegExTypoFix rules
+ */
+function loadTypoFixRules( page ) {
 	var api = new mw.Api();
 	api.get( {
 		action: 'query',
@@ -25,15 +117,19 @@ function getWikiText( page ) {
 		titles: page
 	}, {
 		ok: function ( data ) {
-			processText( data.query.pages[ data.query.pageids[0] ].revisions[0]['*'] );
-			return false;
-                }
+			var     q = data.query,
+				id = q && q.pageids && q.pageids[0],
+				pg = id && q.pages && q.pages[ id ],
+				rv = pg && pg.revisions;
+			if ( rv && rv[0] && rv[0]['*'] ) {
+				processText( rv[0]['*'] );
+			}
+		}
 	} );
 }
 
 if( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ]) !== -1 ) {
 	mw.loader.using( ['mediawiki.api', 'user.options' ], function () {
-		getWikiText( 'Teste' );
-		return false;
+		loadTypoFixRules( 'Project:AutoWikiBrowser/Typos' );
 	} );
 }
