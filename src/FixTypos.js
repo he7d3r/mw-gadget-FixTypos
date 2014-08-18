2abc5e9ae550ed43adfe3e4f6fb57827b2e6328e
Helder <he7d3r@gmail.com>
2014-08-18 18:11:42 -0300

    Inform the license: CC BY-SA 3.0

--- src/FixTypos.js
+++ src/FixTypos.js
@@ -2,7 +2,8 @@
  * Insere um botão na barra de ferramentas de edição para realizar correções tipográficas
  * (Simplificação do [[w:en:User:Cacycle/wikEd.js]])
  * @author: [[w:en:User:Cacycle/wikEd.js]]
- * @author: [[User:Helder.wiki]]
+ * @author: Helder (https://github.com/he7d3r)
+ * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  * @source: [[Special:GlobalUsage/User:Helder.wiki/Tools/FixTypos.js]] ([[File:User:Helder.wiki/Tools/FixTypos.js]])
  */
 /*jslint browser: true, white: true, continue: true, plusplus: true, regexp: true*/
